bed7525ff7e623844fa171023109e571df00ca90
Michael Schenk <mschenk@edgeg.io>
2016-07-02 20:17:17 -0500

    Add -lws2_32 to CGO_LDFLAGS for Window builds

--- script/with-static.sh
+++ script/with-static.sh
@@ -6,6 +6,9 @@ export BUILD="$PWD/vendor/libgit2/build"
 export PCFILE="$BUILD/libgit2.pc"
 
 FLAGS=$(pkg-config --static --libs $PCFILE) || exit 1
+if [[ "$OSTYPE" = "msys" &&  !("-lws2_32" == *"${FLAGS}"*) ]]; then
+  FLAGS="${FLAGS} -lws2_32"
+fi
 export CGO_LDFLAGS="$BUILD/libgit2.a -L$BUILD ${FLAGS}"
 export CGO_CFLAGS="-I$PWD/vendor/libgit2/include"
 
